570ba7e4d212edecf43b1fe712732a376538024f
indigo0086 <indigo0086@gmail.com>
2013-10-07 21:06:54 -0400

    Adding first lesson

---
+++ 01-SimpleApp/app.js
@@ -0,0 +1,18 @@
+var app = angular.module("SimpleApp", []);
+
+app.controller("SimpleController", function($scope){
+//    $scope.greeting = "Hello, World!";
+//    $scope.items = ["Apple", "Orange", "Red", "Blue"]
+//    $scope.person = {
+//        name: "Daniel Pritchett",
+//        age: "24"
+//    }
+//
+//    $scope.update = function() {
+//        var person = $scope.person;
+//        alert(
+//            "Name:" + person.name +
+//            " age: " + person.age + "."
+//        );
+//    }
+})
